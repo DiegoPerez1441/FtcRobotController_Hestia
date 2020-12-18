918846e9697348c431829d9213d10694e1070809
FrancisMorales10 <x.morales10@yahoo.com>
2020-12-17 20:25:33 -0600

    Added Test Comment

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControl.java
@@ -192,7 +192,7 @@ public class DriverControl extends LinearOpMode {
 
             // Both of them return and receive a value from 0.0 - 1.0
             clawServo.setPosition(gamepad1.right_trigger);
-
+            //Test line
 
              /*
         Telemetry Section
