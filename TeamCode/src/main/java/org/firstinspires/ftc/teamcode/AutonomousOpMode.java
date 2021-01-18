48db6ffb7f82dcf649e61d11315abc1f51930c0f
ChristianCastillo12 <75282658+ChristianCastillo12@users.noreply.github.com>
2021-01-18 13:47:13 -0600

    Test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousOpMode.java
@@ -60,7 +60,7 @@ public class AutonomousOpMode extends LinearOpMode {
     //========================================
     // DECLARE OPMODE MEMBERS
     //========================================
-    
+    //Nice Day
     // Misc
     private final ElapsedTime runtime = new ElapsedTime();
 
